9c2161a1aa0829dba592076c747f147246821e53
Jared Mates <jaredmates45@gmail.com>
2022-07-25 20:43:19 -0700

    fetching data based on my input

--- src/index.js
+++ src/index.js
@@ -1,48 +1,64 @@
-// TODO: replace {london} with whatever my input value is
-// TODO: Get lat and Lon and chain the promise to use the results in getting weather data
+const submitForm = document.querySelector(".form");
 
-async function getCityName() {
-  const response = await fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=8726a04f59dfd35caedc4c81c1f18e66",
-    { mode: "cors" }
-  );
-  const cityName = await response.json();
-  console.log(parseInt(cityName[0].lat));
+submitForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let cityName = document.querySelector(".input");
+  getWeatherData(cityName.value);
+  cityName.value = "";
+});
+
+async function getCityCoordinates(city) {
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=8726a04f59dfd35caedc4c81c1f18e66`,
+      { mode: "cors" }
+    );
+    const cityData = await response.json();
+    const lat = cityData[0].lat;
+    const lon = cityData[0].lon;
+    const coordinates = {
+      lat: lat,
+      lon: lon,
+    };
+    return coordinates;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-(async () => {
-  await getCityName();
-})();
+async function getWeatherData(input) {
+  try {
+    const obj = await getCityCoordinates(input);
+    const lat = obj.lat;
+    const lon = obj.lon;
 
-// TODO: &units=metric
-// TODO: &units=imperial
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8726a04f59dfd35caedc4c81c1f18e66&units=metric`,
+      { mode: "cors" }
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+    const dataObj = {
+      description: weatherData.weather[0].description,
+      temp: weatherData.main.temp,
+      feels_like: weatherData.main.feels_like,
+      humidity: weatherData.main.humidity,
+      speed: weatherData.wind.speed,
+      name: weatherData.name,
+    };
 
-// weatherData.weather.description
-// weatherData.main.temp
-// weatherData.main.feels_like
-// weatherData.main.humidity
-// weatherData.wind.speed
-// weatherData.name
-
-async function getWeatherData() {
-  const response = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=8726a04f59dfd35caedc4c81c1f18e66",
-    { mode: "cors" }
-  );
-  const weatherData = await response.json();
-  console.log(weatherData);
+    console.log(dataObj);
+    return dataObj;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-(async () => {
-  await getWeatherData();
-})();
+// (async () => {
+//   await getWeatherData();
+// })();
 
 // TODO: default should be san diego weather have this hardcoded in html I think
 
-let cityName = document.querySelector(".city-name");
-let inputData = cityName.value;
-
-// let Inputdata = () => {
-//   let cityName = document.querySelector(".city-name");
-//   return cityName.value;
-// }
+// TODO: &units=metric
+// TODO: &units=imperial
